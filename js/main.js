ac57ce4ba4cfe93340f40c13bf85ab1d09a825c2
jorgeazapata <jorgeandresz8910@hotmail.com>
2021-08-24 21:10:46 -0500

    2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-window.onload
+var pasillo;
+var lateral;
+var nivel;
+
+var pasillo = 
+var lateral =
+var nivel =
+if ( pasillo >= 1 &&pasillo < 9)
+ {
+ 	alert(pasillo)
+ }
+ else if (lateral==derecho	|| lateral == izquierdo)
+ 	
